b62b10f7f6ed0de91e90e7e5f932b416d46ce0e7
Nasr-Edine DRAI <nasr-edine.drai@external.total.com>
2021-06-08 12:18:49 +0200

    add create new post with form

--- blog/blog/views.py
+++ blog/blog/views.py
@@ -1,5 +1,5 @@
 # from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 
 from .models import Post
 
@@ -14,3 +14,9 @@ class BlogListView(ListView):
 class BlogDetailView(DetailView):
     model = Post
     template_name = "post_detail.html"
+
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'post_new.html'
+    fields = '__all__'
