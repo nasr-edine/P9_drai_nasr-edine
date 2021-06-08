83ab93cd3cc64a34eae074b98bd683a46bef96bf
Nasr-Edine DRAI <nasr-edine.drai@external.total.com>
2021-06-08 13:26:32 +0200

    add a crud to the blog post

--- blog/blog/views.py
+++ blog/blog/views.py
@@ -1,6 +1,6 @@
 # from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView
-
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import Post
 
 # Create your views here.
@@ -20,3 +20,15 @@ class BlogCreateView(CreateView):
     model = Post
     template_name = 'post_new.html'
     fields = '__all__'
+
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = "post_edit.html"
+    fields = ['title', 'body']
+
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = "post_delete.html"
+    success_url = reverse_lazy('home')
