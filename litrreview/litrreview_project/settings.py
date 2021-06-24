6a30d973d1279ce853c890b004a0ef32e699385b
Nasr-Edine DRAI <nasr-edine.drai@external.total.com>
2021-06-24 18:38:07 +0200

    add crispy form to signup page

--- litrreview/litrreview_project/settings.py
+++ litrreview/litrreview_project/settings.py
@@ -21,16 +21,20 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # Local
+    'users',
+    'pages',
+    'tickets',
+
+    # 3rd Party
+    'crispy_forms',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'users',
-    'pages',
-    'crispy_forms',
-    'tickets'
+    'django.contrib.staticfiles'
 ]
 
 MIDDLEWARE = [
@@ -127,3 +131,4 @@ MEDIA_URL = '/media/'
 
 # Path where media is stored
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
